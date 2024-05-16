42978b6d48f1ed8be3b59bd287c603d22668bed3
sAnG404 <sanjupremankunnoth@gmail.com>
2024-05-16 14:21:16 +0530

    socket.io

--- client/script.js
+++ client/script.js
@@ -3,25 +3,26 @@ import { io } from 'socket.io-client';
 const socket = io('http://localhost:3000');
 
 socket.on('connect', () => {
-    startChat(`You are connected with id: ${socket.io}`);
+    startChat(`You are connected to this socket: ${socket.id}`);
 })
 
 socket.on('receive-message', msg => {
     displayMsg(msg, 'receive');
 })
 function startChat(msg){
-    // let msgBox = document.createElement("input");
-    // msgBox.name = ''
+    displayMsg(msg, 'start');
 }
 
 let formChat = document.getElementById('frmChat');
 let chatAreaDiv = document.getElementById('chatarea');
+let btnJoinRoom = document.getElementById('btnRoom')
 
 formChat.addEventListener('submit', e => {
     e.preventDefault();
     if(document.getElementById('txtMsgBox')){
         let msg = document.getElementById('txtMsgBox').value;
-        socket.emit('sendMessage', msg);
+        let room = document.getElementById('roomId').value;
+        socket.emit('sendMessage', msg, room);
         displayMsg(msg, 'send');
     }
     else{
@@ -36,10 +37,18 @@ let displayMsg = (msg, type) => {
         ChatDiv.className = "send";
         chatAreaDiv.appendChild(ChatDiv);
     }
-    else{
+    else if(type == "receive"){
         let ChatDiv = document.createElement('div');
         ChatDiv.innerHTML = msg;
         ChatDiv.className = "receive";
         chatAreaDiv.appendChild(ChatDiv);
     }
+    else{
+        document.getElementById("room").innerHTML = msg;
+    }
 }
+btnJoinRoom.addEventListener('click', ()=>{
+    let roomId = document.getElementById("txtRoom").value;
+    document.getElementById("roomId").value = roomId;
+    socket.emit('join-room', roomId);
+});
